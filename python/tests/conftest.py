30b27a7ffb1d75de7c340e4810f7ddcc24c4b4b9
Nick Crews <nicholas.b.crews@gmail.com>
2023-04-07 10:56:27 -0800

    ref: test: simplify getting fixtures
    We never used the load kwarg, and the
    helper function did not need to be a fixture itself.

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -6,40 +6,30 @@ _TEST_FOLDER = os.path.dirname(__file__)
 FIXTURE_DIR = os.path.join(_TEST_FOLDER, "fixtures")
 
 
-@pytest.fixture(scope="session")
-def get_fixture():
+def get_fixture(filename):
     """Fetch locally stored fixture files."""
-
-    def _get_fixture(filename, load=False):
-        filepath = os.path.join(FIXTURE_DIR, filename)
-        if load:
-            with open(filepath) as fileobj:
-                return fileobj.read()
-        else:
-            return filepath
-
-    return _get_fixture
+    return os.path.join(FIXTURE_DIR, filename)
 
 
 @pytest.fixture()
-def filing_1550126(get_fixture):
+def filing_1550126():
     """Returns the file path for 1550126.fec."""
     return get_fixture("1550126.fec")
 
 
 @pytest.fixture()
-def filing_1550548(get_fixture):
+def filing_1550548():
     """Returns the file path for 1550548.fec."""
     return get_fixture("1550548.fec")
 
 
 @pytest.fixture()
-def filing_1606847(get_fixture):
+def filing_1606847():
     """Returns the file path for 1606847.fec."""
     return get_fixture("1606847.fec")
 
 
 @pytest.fixture()
-def filing_invalid_version(get_fixture):
+def filing_invalid_version():
     """Returns the file path for filing_invalid_version.fec."""
     return get_fixture("filing_invalid_version.fec")
