b55d0799245dc4b04dff87e9062864fa8b661a70
Nick Crews <nicholas.b.crews@gmail.com>
2023-04-09 18:07:32 -0800

    ref: tidy up csv.h
    endOfField() is only used in csv.c so it doesn't need to be declared.

--- src/csv.h
+++ src/csv.h
@@ -27,17 +27,15 @@ void csvParserInit(CSV_LINE_PARSER *parser, STRING *line);
 void processFieldChar(char c, FIELD_INFO *info);
 
 void writeDelimeter(WRITE_CONTEXT *context, char *filename, const char *extension);
-
 void writeNewline(WRITE_CONTEXT *context, char *filename, const char *extension);
-
-static inline int endOfField(char c);
+void writeField(WRITE_CONTEXT *context, char *filename, const char *extension, const char *str, int length, FIELD_INFO *info);
 
 // Read a field, either delimted by ascii28 or comma.
-
+//
 // For ascii28:
 // If both the start and end of the field are `"`
 // then return the field contents inside the quotes.
-
+//
 // For comma::
 // Read a CSV field in-place, modifying line and returning start and
 // end positions of the unescaped field. Since CSV fields are always
@@ -46,8 +44,6 @@ void readField(CSV_LINE_PARSER *parser, int useAscii28);
 
 // Advance past the delimeter and increase the column index
 void advanceField(CSV_LINE_PARSER *parser);
-
-void writeField(WRITE_CONTEXT *context, char *filename, const char *extension, const char *str, int length, FIELD_INFO *info);
 
 // Trim whitespace by adjusting start and end pointers in
 // write context
